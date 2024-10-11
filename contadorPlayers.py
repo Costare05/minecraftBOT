24bd4a4542439ec8acc4da93178b02957ef0a865
Renan Costa <73764927+Costare05@users.noreply.github.com>
2024-10-11 20:30:37 -0300

    Arrumando bug 2.0

--- contadorPlayers.py
+++ contadorPlayers.py
@@ -9,7 +9,7 @@ server_port = 25565            # Porta do servidor Minecraft
 try:
     tempoMAX = 2
     i = tempoMAX
-    while i != 0:
+    while True:
         # Conectando ao servidor Minecraft usando o Query
         server = JavaServer(server_address, server_port)
         query = server.query()
@@ -21,9 +21,9 @@ try:
         else:
             i = tempoMAX
         print(players_online)
-    if i == 0:
-        subprocess.run(["sudo", "shutdown", "-h", "now"], check=True)
         
-    time.sleep(60)
+        if i == 0:
+            subprocess.run(["sudo", "shutdown", "-h", "now"], check=True)
+        time.sleep(60)
 except Exception as e:
     print(f"Erro ao conectar ao servidor: {e}")
